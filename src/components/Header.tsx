c4a1344ca879939794bffbf65eb3ec48c751a0c4
Matt Cabarrubias <monapwc2020@gmail.com>
2025-01-27 09:35:08 +0800

    HEADER: changed projects link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -57,9 +57,9 @@ const Header = () => {
                             <a href="#tech" className="ibm-plex-mono-regular text-gray-500 hover:text-gray-300 text-md py-4 border-transparent transition ease-in-out duration-200">
                                 Tech
                             </a>
-                            <a href="#projects" className="ibm-plex-mono-regular text-gray-500 hover:text-gray-300 text-md py-4 border-transparent transition ease-in-out duration-200">
+                            <Link to="/projects" className="ibm-plex-mono-regular text-gray-500 hover:text-gray-300 text-md py-4 border-transparent transition ease-in-out duration-200">
                                 Projects
-                            </a>
+                            </Link>
                             <a href="#contact" className="ibm-plex-mono-regular text-gray-500 hover:text-gray-300 text-md py-4 border-transparent transition ease-in-out duration-200">
                                 Contact
                             </a>
